7fc6e732f6242ee12ca23bc36f580ebb2e4520de
Amine Loubar <77306622+amine-loubar@users.noreply.github.com>
2024-02-29 15:29:17 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dark_gray: '#121212',
+      }
+    },
+  },
+  plugins: [],
+}
